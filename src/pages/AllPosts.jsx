deb27c9b0e1c61b2ad9363b835e1c0e675b02fde
Suraj5260 <suraj.telrandhe16@gmail.com>
2024-04-09 13:50:17 +0530

    md

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -18,7 +18,7 @@ function AllPosts() {
     // console.log(posts);
 
     return (
-        <div className='w-full py-8'>
+        <div className='w-screen py-8'>
             <Container>
                 <div className='flex flex-wrap'>
                     <div className='w-full p-2'>
@@ -29,7 +29,7 @@ function AllPosts() {
             <Container>
                 <div className='flex flex-wrap'>
                     {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
+                        <div key={post.$id} className='p-2 md:w-1/4 w-1/2'>
                             <PostCard {...post} />
                         </div>
                     ))}
